edd1f6cd1c2f02142f42010da9a08ae053116630
m <139261114+mauriciorfdev@users.noreply.github.com>
2024-11-20 19:11:17 -0300

    added /frontend folder - test vite & chakra ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { Box } from '@chakra-ui/react'
+import { Route, Routes } from 'react-router-dom'
+
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <Box minH={"100vh"}>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<HomePage/>} ></Route>
+        <Route path='create' element={<CreatePage/>} ></Route>
+      </Routes>
+    </Box>
+  )
+}
+
+export default App
