7e3e428632dc4ab894f9b4d7ea507b7e51356765
m <139261114+mauriciorfdev@users.noreply.github.com>
2024-11-29 08:52:52 -0300

    added a Route (path='*', NoMatch Comp) & PostDog with Form (fetch api)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import CreatePage from './pages/CreatePage'
 import NavBar from './components/NavBar'
 import PostDog from './components/PostDog'
 import UpdateDog from './components/updateDog'
+import NoMatch from './components/NoMatch'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <Route path='create' element={<CreatePage />} ></Route>
         <Route path='dog' element={<PostDog />} ></Route>
         <Route path='dog/:id' element={<UpdateDog /> }></Route>
+        <Route path='*' element={<NoMatch />}></Route>
       </Routes>
     </Box>
   )
--- frontend/src/components/PostDog.jsx
+++ frontend/src/components/PostDog.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const PostDog = () => {
-  return (
+  const [name,setName] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch('http://localhost:5000/api/dogs', {
+        method: 'POST',
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({name})
+      })
+      const data = response.json();
+      console.log(data)
+      console.log(JSON.stringify({name}))
+      navigate('/')
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  return (<>
+
     <div>PostDog</div>
-  )
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className='mb-3'>
+        <Form.Label>Dog Name</Form.Label>
+        <Form.Control 
+          type='text' 
+          name='dogname'
+          placeholder='Enter the dog name'
+          value={name}
+          onChange={(e)=>{setName(e.target.value)}}
+        />
+      </Form.Group>
+      <Button type='submit'>Submit</Button>
+    </Form>
+  </>)
 }
 
 export default PostDog
