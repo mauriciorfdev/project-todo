ab432fba66ea84e49dd21ee05243e1b04ee5f841
m <139261114+mauriciorfdev@users.noreply.github.com>
2024-12-02 17:47:29 -0300

    added on DogsTable: Action Buttons for Update/Delete
    Init Form on UpdateDog Component

--- frontend/src/components/DogsTable.jsx
+++ frontend/src/components/DogsTable.jsx
@@ -1,35 +1,48 @@
 import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const DogsTable = () => {
   const [dogs, setDogs] = useState([])
+  const navigate = useNavigate()
 
-  useEffect(()=>{
+  useEffect(() => {
     const fetchDogs = async () => {
-      try{
+      try {
         const resp = await fetch("http://localhost:5000/api/dogs")
         const data = await resp.json()
         setDogs(data)
       }
-      catch(error){
+      catch (error) {
         console.error('error while fetching dogs: ', error.message)
       }
     }
     fetchDogs()
   }, [])
 
+  function handleUpdate(dog) {
+    navigate(`dog/${dog._id}`)
+  }
+
   return (<>
     <div>DogsTable</div>
-    <Table>
+    <Table striped>
       <thead>
         <tr>
-          <th>name</th>
+          <th>Name</th>
+          <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        {dogs.map(dog => 
+        {dogs.map(dog =>
           <tr key={dog.name}>
             <td>{dog.name}</td>
+            <td>
+              <Button onClick={() => { handleUpdate(dog) }} >Update</Button>{' '}
+              <Button variant='dark'>Delete</Button>
+            </td>
+
           </tr>
         )}
       </tbody>
--- frontend/src/components/UpdateDog.jsx
+++ frontend/src/components/UpdateDog.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
 
 const updateDog = () => {
-  return (
+  return (<>
     <div>updateDog</div>
-  )
+    <Form>
+      
+    </Form>
+  </>)
 }
 
 export default updateDog
